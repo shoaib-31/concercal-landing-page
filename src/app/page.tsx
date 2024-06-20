d327567b2cd948c1be41d932c397e7be0f9b1cbc
Shoaib Akhtar <shoaibakmasood@gmail.com>
2024-06-20 22:29:13 +0530

    fix: updated background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,29 @@
 import { motion, AnimatePresence } from "framer-motion";
 import AnimatedText from "../components/AnimatedText";
 import Navbar from "../components/Navbar";
-import Image from "next/image";
 import LoginButton from "../components/LoginButton";
 import Grid from "../components/Grid";
 import Footer from "@/components/Footer";
+import { Spotlight } from "@/components/SpotLight";
 
 export default function Home() {
   return (
-    <main className="flex bg-gradient-to-tr from-black/95 to-slate-800/90 min-h-screen flex-col w-screen overflow-x-hidden items-center">
+    <main className=" flex-col w-screen overflow-x-hidden bg-fixed  bg-black  bg-dot-white/[0.2] relative flex items-center">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-80"
+        fill="white"
+      />
       <Navbar />
       <AnimatePresence>
         <motion.div
-          className=" w-full flex justify-between items-center mt-10 h-[80vh] px-4"
+          className=" w-full flex justify-between items-center h-[90vh] px-4"
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
-          <div className=" flex flex-col w-[50vw] gap-4 justify-center items-center">
+          <div className=" flex flex-col w-full gap-4 justify-center items-center">
             <AnimatedText />
-            <div className=" text-2xl text-white/60  text-center">
+            <div className=" text-2xl w-3/5 text-white/60  text-center">
               Experience the pulse of live music with ease. Discover and track
               your favorite artists&apos; upcoming events effortlessly with our
               intuitive platform.
@@ -32,20 +36,13 @@ export default function Home() {
               className=" transform translate-x-10"
             />
           </div>
-          <Image
-            src="/landing.svg"
-            alt="landing"
-            width={850}
-            height={850}
-            className=" w-[50vw]"
-          />
         </motion.div>
       </AnimatePresence>
       <div className=" w-full text-[9rem] 2xl:text-[10rem] font-medium text-white/5 text-center">
         Sync Your Beats
       </div>
       <Grid />
-      <div className=" w-full text-center font-medium mb-10">
+      <div className=" w-full text-center text-xl font-medium mb-10">
         Live.Love.Listen.
       </div>
       <Footer />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,18 @@
 "use client";
 import React from "react";
 import MySvgComponent from "./AnimatedSvg";
+import { ContactIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className=" w-full p-2">
+    <div className=" w-full p-2 flex justify-between items-center">
       <div className="flex relative items-center p-2">
         <MySvgComponent />
-        <div className="text-whtie text-3xl font-semibold">concetcal.com</div>
       </div>
+      <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+        <ContactIcon size={24} />
+        <span>Contact Us</span>
+      </button>
     </div>
   );
 };
