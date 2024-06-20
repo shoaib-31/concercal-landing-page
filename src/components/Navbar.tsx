e3a0800064c544bdd4a8de10aefff36dcdb29de4
Shoaib Akhtar <shoaibakmasood@gmail.com>
2024-06-20 22:35:45 +0530

    fix: updated background

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,7 @@ const Navbar = () => {
       <div className="flex relative items-center p-2">
         <MySvgComponent />
       </div>
-      <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-        <ContactIcon size={24} />
+      <button className="bg-gray-800/50 hover:bg-gray-600/50 text-white font-bold py-2 px-4 rounded inline-flex items-center">
         <span>Contact Us</span>
       </button>
     </div>
